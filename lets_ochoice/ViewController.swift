34d4df386e088004e11cc13eddf0dcc2bf95cc0d
LeeHongYul <ryullee0911@gmail.com>
2025-01-13 22:14:33 +0900

    Initial Commit

---
+++ lets_ochoice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lets_ochoice
+//
+//  Created by homechoic on 1/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
