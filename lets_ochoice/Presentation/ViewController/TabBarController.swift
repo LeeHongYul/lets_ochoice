df865fbbbda8cc2226b58ecd2af78588767fffc0
LeeHongYul <ryullee0911@gmail.com>
2025-01-26 15:33:39 +0900

    Update: TabBarView refact

--- lets_ochoice/Presentation/ViewController/TabBarController.swift
+++ lets_ochoice/Presentation/ViewController/TabBarController.swift
@@ -5,20 +5,30 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Create view controllers for each tab
-        let detailViewController = MainViewController() // Example ID, you can pass your actual ID
-        detailViewController.title = "Detail" // Set the title for the tab
+        view.backgroundColor = .black
         
-        let anotherViewController = MyPageViewController() // Your other view controller
-        anotherViewController.title = "Other"
+        tabBar.unselectedItemTintColor = .gray
         
-        // Assign the view controllers to the tab bar controller
-        self.viewControllers = [detailViewController, anotherViewController]
+        tabBar.tintColor = .red
+        
+        setupViewControllers()
+    }
+    
+    private func setupViewControllers() {
+       
+        let detailViewController = MainViewController()
+        detailViewController.title = "홈"
+        
+        let myPageViewController = MyPageViewController()
+        myPageViewController.title = "VOD"
+        
+    
+        self.viewControllers = [detailViewController, myPageViewController]
         
         // Customize Tab Bar appearance if needed
         let tabBarItems = self.tabBar.items
-        tabBarItems?[0].image = UIImage(systemName: "film.fill")
-        tabBarItems?[1].image = UIImage(systemName: "star.fill")
-    }
+        tabBarItems?[0].image = UIImage(systemName: "house.fill")
+        tabBarItems?[1].image = UIImage(systemName: "tv")
+        }
 }
 
